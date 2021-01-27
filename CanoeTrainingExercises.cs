23609d0741b6268f302dd06e12ac221df7da9c7a
franRob <37939179+jmjp775@users.noreply.github.com>
2021-01-27 11:25:51 -0500

    don't forget to create a folder

---
+++ CanoeTrainingExercises.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializeTutorial.Entities
+{
+    public class CanoeTrainingExercises
+    {
+        /// <summary>
+        /// Where the canoe training object is stored
+        /// Similar to ClientEntry
+        /// </summary>
+        public int CanoeTrainingID { get; set; }
+        public string CanoeErg { get; set; }
+        public string XcSkiing { get; set; }
+        public string Running { get; set; }
+        public List<StrengthExercises> StrengthExercisesList { get; set; } = new List<StrengthExercises>();
+    }
+}
